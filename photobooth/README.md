1fb9680bd5dd58907896fceb87b97bbe12365f3f
happytaeyoon <s23021@gsm.hs.kr>
2024-10-29 17:23:42 +0900

    set file setting

---
+++ photobooth/README.md
@@ -0,0 +1,1 @@
+## Filterism PhotoBooth
---
+++ photobooth/src/App.js
@@ -0,0 +1,11 @@
+import Test from './components/Test/Test.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
